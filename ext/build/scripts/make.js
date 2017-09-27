cac378c703c0078663fd06a2f90d6bf306de023a
Christian Kleinhuis <ck@froso.de>
2017-09-27 22:47:15 +0200

    fix cwd()

--- ext/build/scripts/make.js
+++ ext/build/scripts/make.js
@@ -83,8 +83,8 @@ logger.log('Execute Build')
 
 if (CLEAN) {
     logger.info('Cleaning build folders...')
-    rimraf.sync(path.join(process.cwd, 'dist'), handleError)
-    rimraf.sync(path.join(process.cwd, Constants.TEST_REPORT_FOLDER), handleError)
+    rimraf.sync(path.join(process.cwd(), 'dist'), handleError)
+    rimraf.sync(path.join(process.cwd(), Constants.TEST_REPORT_FOLDER), handleError)
     logger.info('.. finished')
 }
 
