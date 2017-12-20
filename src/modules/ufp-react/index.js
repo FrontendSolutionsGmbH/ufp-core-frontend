627c67e53e9acc41653af8d18975a212f32b7c2f
Christian Kleinhuis <ck@froso.de>
2017-12-20 17:24:27 +0100

    ufplist

--- src/modules/ufp-react/index.js
+++ src/modules/ufp-react/index.js
@@ -1,4 +1,5 @@
 import _Runfest from './Runfest'
+import _UfpList from './view/components/UfpList'
 
 export const Runfest = _Runfest
 export const ReactRunfest = _Runfest
@@ -6,3 +7,4 @@ export const ReactRunfest = _Runfest
 export const registerRootSibbling = _Runfest.registerRootSibbling
 export const registerRootProvider = _Runfest.registerProvider
 export const appCreatorFunction = _Runfest.register
+export const UfpList = _UfpList
