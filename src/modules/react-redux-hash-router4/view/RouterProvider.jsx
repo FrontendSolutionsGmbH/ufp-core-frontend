e7c5539e7ab7fc0cdcf83beddf8cd2e094098884
Christian Kleinhuis <ck@digitalgott.de>
2018-04-09 22:57:35 +0200

    linting, and missing dep

--- src/modules/react-redux-hash-router4/view/RouterProvider.jsx
+++ src/modules/react-redux-hash-router4/view/RouterProvider.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import createHistory from 'history/createBrowserHistory'
 import {ConnectedRouter} from 'react-router-redux'
 const history = createHistory()
@@ -10,6 +11,9 @@ const RouterProvider = ({children}) => {
             {children}
         </ConnectedRouter>
     )
+}
+RouterProvider.propTypes = {
+    children: PropTypes.node.isRequired
 }
 
 export default RouterProvider
