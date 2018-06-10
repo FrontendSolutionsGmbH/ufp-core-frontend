f3266bd6829612828857238904e56c3f3b5d99d8
Christian Kleinhuis <ck@digitalgott.de>
2018-06-10 21:11:01 +0200

    rename

--- src/modules/react-redux-hash-router4/HistoryX.js
+++
@@ -1,4 +0,0 @@
-import createHashHistory from 'history/createHashHistory'
-// Create a history of your choosing (we're using a browser history in this case)
-const History = createHashHistory()
-export default History
