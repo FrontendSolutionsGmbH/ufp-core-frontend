51c240d0c41f721b72e34b2997ffd559626a63d8
Christian Kleinhuis <ck@digitalgott.de>
2018-09-04 00:15:52 +0200

    various

---
+++ bin/commands/compileCommand.js
@@ -0,0 +1,21 @@
+const child_process = require('child_process')
+const package = require('../../package.json')
+const logger = require('../../ext/build/lib/Logger2')('ufp-start')
+
+// my-module.js
+exports.command = ['l', 'lint']
+
+exports.describe = 'lint fix'
+
+exports.builder = {}
+
+exports.handler = function (argv) {
+    // do something with argv.
+
+    logger.info('starting development server ')
+    child_process.execSync(package.scripts['ufp-lint:fix'], {
+        cwd: process.cwd(),
+        stdio: 'inherit'
+    })
+
+}
