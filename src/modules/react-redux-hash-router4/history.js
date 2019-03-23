edef77bdd4ba60417a3a9b2ec6a6680ed28afe66
Christian Kleinhuis <ck@digitalgott.de>
2019-03-23 15:50:57 +0100

    no message

--- src/modules/react-redux-hash-router4/history.js
+++ src/modules/react-redux-hash-router4/history.js
@@ -1,5 +1,5 @@
 // import createHistory from "history/createBrowserHistory";
 
-import {createBrowserHistory,createHashHistory} from 'history'
+import {createHashHistory} from 'history'
 // Create a history of your choosing (we're using a browser history in this case)
 export default createHashHistory()
